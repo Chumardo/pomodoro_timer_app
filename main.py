8d971ad2f9f1ab7c099f418c3418054d02a86feb
chumardo <dachigiorgadze98@gmail.com>
2022-08-20 22:29:40 +0400

    packages and constants

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import math
+
+# Constants
+PINK = "#e2979c"
+RED = "red"
+GREEN = "green"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
