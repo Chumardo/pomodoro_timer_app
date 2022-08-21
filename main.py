f243bd8986122d9c1161d4966179855999b4db77
chumardo <dachigiorgadze98@gmail.com>
2022-08-21 11:40:14 +0400

    countdown method and start button

--- main.py
+++ main.py
@@ -13,6 +13,29 @@ LONG_BREAK_MIN = 20
 reps = 0
 timer = None
 
+# Timer Mechanism
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    print(reps)
+    count_down(work_sec)
+
+def count_down(count):
+    
+    count_min = math.floor(count / 60)
+    count_sec = count % 60 
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+        
+    
+    
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
 
 # GUI Setup
 window = Tk()
@@ -29,7 +52,7 @@ timer_text = canvas.create_text(100, 140, text="00:00", fill="white", font=(FONT
 timer_label = Label(text="Timer", font=(FONT_NAME, 40), fg=GREEN, highlightcolor="black", bg="black")
 timer_label.grid(column=1, row=0)
 
-btn_start = Button(text="Start", highlightthickness=0, bg=RED, fg="white")
+btn_start = Button(text="Start", highlightthickness=0, command=start_timer(), bg=RED, fg="white")
 btn_start.grid(column=0, row=2)
 
 btn_reset = Button(text="Reset", highlightthickness=0, bg=GREEN, fg="white")
